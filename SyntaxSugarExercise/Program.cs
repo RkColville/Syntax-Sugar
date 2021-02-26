9c14c603614e7631be8c7f74a97fc5fa397729a7
Robert Colville <rkcolville@gmail.com>
2021-02-25 20:52:21 -0600

    initial commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = "answer";
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+
+        else
+            {
+                response = answer + "greater than or equal to nine";
+            }
+
+            Console.WriteLine(response);
+
+            response = (answer < 9) ? "answer + is less than nine" : "answer + greater than or equal to nine";
+
+
+
+
+
+
+
+
+
+        }
+        
+
+        
+        
+    }
+}
